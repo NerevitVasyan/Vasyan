dde29fb0dcad8af21db9755931de859da0725be2
user <user@A12COMP1>
2018-05-14 20:27:14 +0300

    add class person

--- Mykola/Mykola/Program.cs
+++ Mykola/Mykola/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace Mykola
 {
+    class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
